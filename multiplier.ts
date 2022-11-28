6826b903681924b205b6ee850309b3b2037e55df
joscord <josecorderodev@gmail.com>
2022-11-28 16:17:00 -0300

    feat: add script to package.json. Define multiply function

---
+++ multiplier.ts
@@ -0,0 +1,7 @@
+// Especificamos el tipo de una variable usando : si esto no se especifica el tipo es any y puede ser cualquier cosa
+const multiply = (a: number, b: number) => a * b
+
+// La función puede sólo aceptar argumentos que sean de tipo number sino dará error
+console.log(multiply(5, 2))
+
+
