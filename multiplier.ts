03d03d95e3fcb48452170f0f242d5714ed080fec
joscord <josecorderodev@gmail.com>
2022-11-28 16:44:33 -0300

    feat: create calculator.ts

--- multiplier.ts
+++ multiplier.ts
@@ -2,6 +2,6 @@
 const multiply = (a: number, b: number) => a * b
 
 // La función puede sólo aceptar argumentos que sean de tipo number sino dará error
-console.log(multiply(5, 2))
+console.log(multiply(5, 3))
 
 
