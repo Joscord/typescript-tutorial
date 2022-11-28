28f1a1d9ea14706f55bf3802d0ef6c863b44458a
joscord <josecorderodev@gmail.com>
2022-11-28 17:25:32 -0300

    feat: refactor multiplier.ts to pass arguments through command line

--- multiplier.ts
+++ multiplier.ts
@@ -1,7 +1,8 @@
 // Especificamos el tipo de una variable usando : si esto no se especifica el tipo es any y puede ser cualquier cosa
 const multiply = (a: number, b: number) => a * b
 
-// La función puede sólo aceptar argumentos que sean de tipo number sino dará error
-console.log(multiply(5, 3))
+// Podemos obtener los argumentos del arreglo de la línea de comandos. El primer argumento de este arreglo es el ejecutor, el siguiente el path de archivo y los que siguen los argumentos que pasamos por línea de comandos
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
 
-
+console.log(multiply(a, b))
