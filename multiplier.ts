bdc93798493f0b8b97950bda4f54a5e448ed2daa
joscord <josecorderodev@gmail.com>
2022-11-28 17:54:34 -0300

    feat: modify multiplier.ts. Write function to check arguments passed through command line

--- multiplier.ts
+++ multiplier.ts
@@ -1,8 +1,19 @@
 // Especificamos el tipo de una variable usando : si esto no se especifica el tipo es any y puede ser cualquier cosa
 const multiply = (a: number, b: number) => a * b
 
-// Podemos obtener los argumentos del arreglo de la línea de comandos. El primer argumento de este arreglo es el ejecutor, el siguiente el path de archivo y los que siguen los argumentos que pasamos por línea de comandos
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
+// Debemos definir esta función porque dado que typescript no hace el checkeo en runtime es posible que pasemos argumentos que no son esperados por la función, por ejemplo intentar multiplicar 3 + "hola mundo" o 3 * 4 * 6
+const parseArguments = (args:Array<number|string>):Array<number> => {
+    if (!(args.length === 4)) throw new Error("Wrong number of arguments")
+    const firstNumber: number = Number(process.argv[2])
+    const secondNumber: number = Number(process.argv[3])
+    if(!isNaN(firstNumber) && !isNaN(secondNumber)) return [firstNumber, secondNumber]
+    throw new Error("Wrong type of arguments")
+}
+
+// Podemos obtener los argumentos del arreglo de la línea de comandos. El primer argumento de este arreglo es el ejecutor, el siguiente el path de archivo y los que siguen los argumentos que pasamos por línea de comandos.
+const cleanArguments = parseArguments(process.argv)
+
+const a: number = cleanArguments[0]
+const b: number = cleanArguments[1]
 
 console.log(multiply(a, b))
